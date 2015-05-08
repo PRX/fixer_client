bdb782366aa9a28d3095722e5184303782aa629a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-05-08 10:09:45 -0400

    transcode example

---
+++ examples/transcode.rb
@@ -0,0 +1,28 @@
+require 'dotenv'
+Dotenv.load '.env-fixer_client'
+
+require 'fixer_client'
+
+Fixer.configure do |c|
+  c.client_id = ENV['FIXER_CLIENT_ID']
+  c.client_secret = ENV['FIXER_CLIENT_SECRET']
+  c.endpoint = 'http://fixer-dev.prx.dev/api/'
+end
+
+client = Fixer::Client.new
+
+job = {
+  job: {
+    original: 's3://test-fixer/audio.wav',
+    job_type: 'audio',
+    tasks: [
+      {
+        task_type: 'transcode',
+        options: { format: 'mp3', bit_rate: 64, sample_rate: 44100 },
+        result: 's3://test-fixer/audio.mp3'
+      }
+    ]
+  }
+}
+
+j = client.jobs.create(job)
